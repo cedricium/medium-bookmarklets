86dfb1230ac49caada7efb21f1e48fd7785b003d
Cedricium <amaya.cedric@gmail.com>
2017-11-21 01:34:22 -0800

    Initialize git with v0.0.1 of M-B

---
+++ src/background.js
@@ -0,0 +1,5 @@
+(function () {
+  let domains = ['medium.com'];
+
+  browser.storage.local.set({domains});
+})();
---
+++ src/bookmarklets.js
@@ -0,0 +1,158 @@
+
+(function () {
+  // let domains = [
+  //   'medium.com',
+  //   'hackernoon.com',
+  //   'medium.freecodecamp.org',
+  //   'engineering.instagram.com'
+  // ];
+
+  browser.storage.local.get('domains')
+    .then((domainList) => {
+      startScript(domainList);
+  });
+
+  // let currentURL = location.href;
+  // let currentHostname = location.hostname;
+
+  // if (!domains.includes(currentHostname))
+  //   return;
+
+  // initialize();
+  // window.setInterval(function() {
+  //   if (currentURL !== location.href) {
+  //     currentURL = location.href;
+  //     location.reload(false);
+  //   }
+  // }, 250);
+})();
+
+
+
+function startScript(domainData) {
+  console.log(domainData);
+
+  let domains = domainData.domains;
+
+  let currentURL = location.href;
+  let currentHostname = location.hostname;
+
+  if (!domains.includes(currentHostname))
+    return;
+
+  initialize();
+  window.setInterval(function() {
+    if (currentURL !== location.href) {
+      currentURL = location.href;
+      location.reload(false);
+    }
+  }, 250);
+}
+
+
+let currentBookmarklet = '';
+
+
+function onError(err) {
+  console.error(err);
+}
+
+
+function initialize() {
+  let gettingAllBookmarklets = browser.storage.local.get(null);
+  gettingAllBookmarklets.then((results) => {
+    addBookmarklet();
+    let url = location.href;
+
+    if (url in results) {
+      let bookmarklet = JSON.parse(results[url]);
+      displayBookmarklet(bookmarklet.id);
+    }
+  }, onError);
+}
+
+
+function addBookmarklet(){
+  document.addEventListener('keyup', function(e) {
+    let selectionData = getSelectionData();
+    if (e.keyCode === 66 && selectionData.text) {
+      if (currentBookmarklet !== selectionData.targetElement.id) {
+        let bookmarklet = {
+          id: selectionData.targetElement.id,
+          url: location.href,
+          text: selectionData.text
+        };
+
+        console.log('unique bookmarklet added');
+
+        storeBookmarklet(bookmarklet);
+      }
+    }
+  });
+}
+
+
+function removeBookmarkletDot(id) {
+  let bookmarklet = document.getElementById(id);
+  bookmarklet.classList.remove('mb__bookmarklet');
+}
+
+
+function deleteBookmarklet(e) {
+  removeBookmarkletDot(e.target.id);
+
+  // TODO: delete bookmarklet for given url (set url's value to null? => look up `storage.local.set()` docs)
+
+  currentBookmarklet = null;
+}
+
+
+function storeBookmarklet(bookmarkletObj) {
+  let url = bookmarkletObj.url;
+
+  let storingBookmarklet = browser.storage.local.set({[url]: [JSON.stringify(bookmarkletObj)]});
+  storingBookmarklet.then(() => {
+    displayBookmarklet(bookmarkletObj.id);
+  }, onError);
+}
+
+
+function displayBookmarklet(bookmarkletID) {
+  let previousBookmarklet = document.querySelector('.mb__bookmarklet');
+  if (previousBookmarklet)
+    removeBookmarkletDot(previousBookmarklet.id);
+
+  currentBookmarklet = bookmarkletID;
+  let bookmarklet = document.getElementById(bookmarkletID);
+  bookmarklet.classList.add('mb__bookmarklet');
+  bookmarklet.onclick = deleteBookmarklet;
+}
+
+
+function getSelectionData() {
+  let selectionObj = null;
+  let targetElement = null;
+  let text = '';
+
+  if (typeof window.getSelection !== 'undefined') {
+    selectionObj = window.getSelection();
+    text = selectionObj.toString();
+    targetElement = (selectionObj.anchorNode.nodeType === 1) ? selectionObj.anchorNode : selectionObj.anchorNode.parentElement;
+  }
+
+  if (!targetElement.classList.contains('graf')) {
+    console.log('Please highlight article text only.');
+    return;
+  }
+
+  let selectionInfo = {
+    "targetElement": targetElement,
+    "text": text
+  }
+
+  return selectionInfo;
+}
+
+
+// initialize();
+// addBookmarklet();
