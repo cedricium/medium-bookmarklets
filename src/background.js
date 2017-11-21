14bda2b64f7845e935e2ca0a230c36e32339c56d
Cedricium <amaya.cedric@gmail.com>
2017-11-21 11:07:51 -0800

    Set initial domain list if one does not exist
    With this commit, the background script immediately checks for the dom-
    ain list and if one is not found, it sets one (this scenario usually
    occurs once when the extension is first provisioned and any subsequent
    updates).

--- src/background.js
+++ src/background.js
@@ -1,5 +1,20 @@
 (function () {
-  let domains = ['medium.com'];
+  let gettingDomains = browser.storage.local.get('domains');
+  gettingDomains.then((storedDomains) => {
+    console.log(storedDomains);
+    if (Object.keys(storedDomains).length === 0) {
+      setInitialDomains();
+    } else {
+      testDomains();
+    }
+  }, onError);
 
-  browser.storage.local.set({domains});
+  function setInitialDomains() {
+    let domains = ['medium.com'];
+    browser.storage.local.set({domains});
+  }
 })();
+
+function onError(err) {
+  console.error(err);
+}
