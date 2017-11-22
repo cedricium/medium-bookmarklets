d7261688ac28fbadf1005c8c1051ec916ab2bdf1
Cedricium <amaya.cedric@gmail.com>
2017-11-22 00:16:45 -0800

    Designed and created POC for Settings page view

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -45,3 +45,35 @@ function createCard(obj) {
 function openBookmarklet(url) {
   let updating = browser.tabs.update({"url": url});
 }
+
+
+document.querySelector('p.nav').addEventListener('click', setSettingsView);
+
+function setSettingsView() {
+  let mainContentDiv = document.querySelector('div.main-content');
+  mainContentDiv.innerHTML = `
+    <div class="settings-container">
+      <div class="settings-section domains>
+        <p class="settings-section--title">Domains</p>
+        <small class="settings-section--desc">
+          Medium publications that do not have the 'medium.com' domain must be added here
+          in order for Medium Bookmarklets to work on them.
+        </small>
+        <input type="button" class="outline" value="+ Add Domain" />
+
+        <div class="domain-input">
+          <input type="text" class="string domain" placeholder="e.g. medium.com" />
+          <input type="button" class="outline remove" value="✕ Remove" />
+        </div>
+      </div>
+
+      <button class="button save">Save</button>
+    </div>
+  `;
+
+  document.querySelector('button.button.restore').addEventListener('click', setMainView);
+}
+
+function setMainView() {
+  location.reload(true);
+}
