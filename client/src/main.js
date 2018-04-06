f5036c0862f9f9ac967656f5bbf70f7e3ea1d5dc
Cedricium <amaya.cedric@gmail.com>
2018-04-05 18:39:46 -0700

    Initial setup of Vue.js redesign

---
+++ client/src/main.js
@@ -0,0 +1,22 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import router from './router';
+import Buefy from 'buefy';
+import 'buefy/lib/buefy.css';
+
+Vue.config.productionTip = false;
+
+Vue.use(Buefy, {
+  defaulIconPack: 'fa' // OR fas (solid), far (regular)
+});
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+});
